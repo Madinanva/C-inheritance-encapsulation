f95aa507dfa9fe5a9b02df31e9498b90cf4678b9
Madina <madinarn@code.edu.az>
2022-07-12 11:03:33 +0400

    first commit

---
+++ Invoice.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    public class Invoice
+    {
+        private string account;
+        private string customer;
+        private string provider;
+        public string Article { get; set; }
+        public int Quantity { get; set; }
+        public Invoice()
+        {
+
+        }
+          public Invoice(string account, string customer, string provider)
+        {
+            this.account = account;
+            this.customer = customer; 
+            this.provider = provider;
+        }
+          public double CostCalculation(bool needEdv, double cost, double count)
+        {
+           double edv = cost * 0.18;
+
+            if(needEdv == true)
+            {
+               cost = cost * count + edv;
+            }
+            else
+            {
+               cost = cost * count;
+            }
+            return cost;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //    string word = "basic";
+            //    ProDocumentProgram pro = new ProDocumentProgram();
+            //    ExpertDocumentProgram expert = new ExpertDocumentProgram();
+            //    DocumentProgram basic = new DocumentProgram();
+
+            //    if (word == "pro")
+            //    {
+            //        pro.EditDocument();
+            //        pro.SaveDocument();
+            //        pro.OpenDocument();
+            //    }
+            //    else if(word == "basic")
+            //    {
+            //        basic.EditDocument();
+            //        basic.SaveDocument();
+            //        basic.OpenDocument();
+            //    }
+            //    else if(word == "expert")
+            //    {
+            //        expert.EditDocument();
+            //        expert.SaveDocument();
+            //        expert.OpenDocument();
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("nothing");
+            //    }
+
+            var obj = new Invoice();
+            obj.Article = "LapTop";
+
+            Invoice invoice = new Invoice("678904", "Alex", "Foxtrot");
+
+
+            obj.Article = "USB-hab";
+            obj.Quantity = 6;
+
+            double result = obj.CostCalculation(true, 1400, 4);
+            Console.WriteLine(result);
+        }
+    
+    }
+}
